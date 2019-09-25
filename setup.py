fcfc22294b09f02ed10cd93559d90e0e08dd8266
code-up-all <53559360+code-up-all@users.noreply.github.com>
2019-09-25 06:28:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,12 +22,12 @@ def download_data(url, zipped_path):
         file1.flush()
 
   # Extract file.
-  zip_file = gzip.GzipFile(zipped_path, 'w')
+  #zip_file = gzip.GzipFile(zipped_path, 'w')
   #zip_file.extractall('')
   #zip_file.close()
  
-  handle = gzip.open(zipped_path)
-  with gzip.open(zip_file, 'w') as out:
+  handle = gzip.open(zipped_path, 'r')
+  with gzip.open(zipped_path, 'w') as out:
     for line in handle:
       out.write(line)
 
